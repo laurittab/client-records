bc650da461ef0206a787fa807bb1256e68022609
ReetzOnline <lauritta@post.com>
2022-03-18 05:02:26 +0000

    adding project from glitch.com account https://sample-client-records-api.glitch.me

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+import Button from "@mui/material/Button";
+import ListIcon from "@mui/icons-material/List";
+
+function App() {
+  const [sample, setSample] = useState("");
+
+  function recordsHandler(event) {
+    //var resData;
+    axios
+      .get("https://1-a-node-app-for-medical-records.glitch.me/records")
+      .then((response) => {
+        var resData = response.data;
+        var data = JSON.stringify(resData.data);
+        setSample(data);
+      });
+    event.preventDefault();
+  }
+
+  return (
+    <div style={{ margin: "50px" }}>
+      <Button
+        type="button"
+        variant="contained"
+        color="primary"
+        size="large"
+        startIcon={<ListIcon />}
+        disabled={false}
+        onClick={recordsHandler}
+      >
+        Get a Sample List of Client Records
+      </Button>
+      <br />
+      <br />
+      <div>{sample}</div>
+    </div>
+  );
+}
+export default App;
